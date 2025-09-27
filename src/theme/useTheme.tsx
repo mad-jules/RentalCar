016300b2d531ce5bcda2dd9e2d92854540d5df71
mad-jules <yuliia.pechera@gmail.com>
2025-09-28 01:10:41 +0300

    Fill theme variables

---
+++ src/theme/useTheme.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ThemeContext } from './ThemeProvider';
+
+export const useTheme = () => {
+  const ctx = React.useContext(ThemeContext);
+  if (!ctx) {
+    throw new Error('useTheme must be used within ThemeProvider');
+  }
+  return ctx;
+};
